b53104d83f8f0ea2b8bee5c35a8b68c0f54a3448
daniel <djp77789@gmail.com>
2023-02-06 11:50:48 -0800

    Got circleKernal working for position 0,0, but does not work when moving the circle yet.

--- pgmProcess.cu
+++ pgmProcess.cu
@@ -28,13 +28,11 @@ __global__ void dPgmDrawCircle(int *pixels, int numRows, int numCols, int center
     int ix   = blockIdx.x*blockDim.x + threadIdx.x;
     int iy   = blockIdx.y*blockDim.y + threadIdx.y;
     int idx = iy*numCols + ix;
-
-    int p1[2] = {iy, ix};
+    
+    int p1[2] = {(int)blockIdx.x, ix % 10};
     int p2[2] = {centerRow, centerCol};
     float dis = distance(p1, p2);
-    //    printf("%f ", dis);
-
-    if((float)radius > dis ) {
+    if((float)radius >= dis ) {
         pixels[idx] = 255;
     }
 
--- pgmUtility.cu
+++ pgmUtility.cu
@@ -90,7 +90,7 @@ int pgmWrite( const char **header, const int *pixels, int numRows, int numCols, 
 //     int * d_temp = 0;
 //     cudaMalloc((void **) &d_temp, num_bytes);
 //     cudaMemcpy( d_temp, temp, num_bytes, cudaMemcpyHostToDevice );
-//     dPgmDrawCircle<<<numRows, numCols>>>(d_temp, numRows, numCols, 2, 2, 20, header);
+//     dPgmDrawCircle<<<numRows, numCols>>>(d_temp, numRows, numCols, 0, 0, 2, header);
 //     cudaMemcpy( temp, d_temp, num_bytes, cudaMemcpyDeviceToHost );
 
 
